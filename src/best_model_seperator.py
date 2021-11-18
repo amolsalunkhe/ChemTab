ae6e9c83e61bcc709d0367cd5ff2945745fcdfcf
Dwyer Deighan <dwyerdei@buffalo.edu>
2021-11-18 02:54:55 +0000

    commiting code for proper variable name recording & zmix detection

--- src/best_model_seperator.py
+++ src/best_model_seperator.py
@@ -21,25 +21,34 @@ bestModel, experimentSettings = exprExec.modelFactory.openBestModel()
 dm.createTrainTestData(experimentSettings['dataSetMethod'], experimentSettings['noOfCpv'],
                        experimentSettings['ipscaler'], experimentSettings['opscaler'])
 
-#print(sys.argv[1])
 composite_model = bestModel#keras.models.load_model(sys.argv[1])
 composite_model.summary()
 
 decomp_dir = f'./PCDNNV2_decomp'
-os.system(f'mkdir {decomp_dir}')
+os.system(f'rm -r {decomp_dir}; mkdir {decomp_dir}')
 
-get_layer = lambda model, name: [layer for layer in model.layers if layer.name==name][0]
+get_layers_by_name = lambda model: {layer.name: layer for layer in model.layers}
+layers_by_name = get_layers_by_name(composite_model)
 
-linear_embedder = get_layer(composite_model, 'linear_embedding')
-w = np.asarray(get_layer(linear_embedder, 'linear_embedding').weights[0])
+linear_embedder = layers_by_name['linear_embedding']
+w = np.asarray(get_layers_by_name(linear_embedder)['linear_embedding'].weights[0])
 print(f'linear embedder weights shape: {w.shape}') # shape is [53, nCPV]
 
-weight_df = pd.DataFrame(w)
-weight_df['VarName'] = dm.input_data_cols
-weight_df.to_csv(f'{decomp_dir}/weights.csv', index=False)
+CPV_names = [f'CPV_{i}' for i in range(w.shape[1])]
+
+if 'zmix' in layers_by_name:
+    #raise NotImplementedError('you need to get the actual weights for zmix here')
+    zmix_weights = np.random.normal(size=(w.shape[0],1))
+    w = np.concatenate([zmix_weights, w],axis=1) # checked on 10/10/21: that zmix comes first
+    CPV_names = ['zmix'] + CPV_names
+weight_df = pd.DataFrame(w, index=dm.input_data_cols, columns=CPV_names) # dm.input_data_cols is why we recreate training data
+weight_df.to_csv(f'{decomp_dir}/weights.csv', index=True, header=True)
 
 #np.savetxt(f'{decomp_dir}/weights.csv', w, delimiter=',')
 #linear_embedder.save(f'{decomp_dir}/linear_embedding')
 
-regressor = get_layer(composite_model, 'prediction')
-regressor.save(f'{decomp_dir}/regressor')
+regressor = layers_by_name['prediction']
+wrapper = keras.models.Sequential()
+wrapper.add(keras.layers.Input(shape=regressor.input_shape[1:], name='input_1'))
+wrapper.add(regressor)
+wrapper.save(f'{decomp_dir}/regressor')
