50ffaea71f34b88ec8ddd9c13f0196669dd9e7d5
Dwyer Deighan <dwyerdei@buffalo.edu>
2022-03-02 18:20:49 +0000

    updated best model seperator to work with dynamic source prediction

--- src/best_model_seperator.py
+++ src/best_model_seperator.py
@@ -37,7 +37,7 @@ print(f'linear embedder weights shape: {w.shape}') # shape is [53, nCPV]
 CPV_names = [f'CPV_{i}' for i in range(w.shape[1])]
 
 if 'zmix' in layers_by_name:
-    #raise NotImplementedError('you need to get the actual weights for zmix here')
+    raise NotImplementedError('you need to get the actual weights for zmix here')
     zmix_weights = np.random.normal(size=(w.shape[0],1))
     w = np.concatenate([zmix_weights, w],axis=1) # checked on 10/10/21: that zmix comes first
     CPV_names = ['zmix'] + CPV_names
@@ -47,8 +47,9 @@ weight_df.to_csv(f'{decomp_dir}/weights.csv', index=True, header=True)
 #np.savetxt(f'{decomp_dir}/weights.csv', w, delimiter=',')
 #linear_embedder.save(f'{decomp_dir}/linear_embedding')
 
-regressor = layers_by_name['prediction']
-wrapper = keras.models.Sequential()
-wrapper.add(keras.layers.Input(shape=regressor.input_shape[1:], name='input_1'))
-wrapper.add(regressor)
+regressor = layers_by_name['regressor']
+input_ = keras.layers.Input(shape=regressor.input_shape[1:], name='input_1')
+wrapper = keras.models.Model(inputs=input_, outputs=regressor(input_))
+#wrapper.add(keras.layers.Input(shape=regressor.input_shape[1:], name='input_1'))
+#wrapper.add(regressor)
 wrapper.save(f'{decomp_dir}/regressor')
