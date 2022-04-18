e6e7f331fd2b96927e8c21f4369917a8cc5970c5
Dwyer Deighan <dwyerdei@buffalo.edu>
2022-04-18 16:26:05 +0000

    finally integrated previous long_train.py with existing optuna train so I should now get the best of both worlds

--- src/optuna_train.py
+++ src/optuna_train.py
@@ -5,6 +5,7 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 from tensorflow import keras
 from tensorflow.keras import layers as L
+from copy import deepcopy
 from main import *
 import optuna
 import sys
@@ -15,7 +16,10 @@ field_names = lambda x: list(vars(x).keys())
 debug_mode = False#True
 if debug_mode: print('debugging!', file=sys.stderr)
 
-def main(cfg):
+def main(cfg={}):
+    # update default template with passed config
+    full_cfg = deepcopy(main.default_cfg)
+    full_cfg.update(cfg)
 
     #Prepare the DataFrame that will be used downstream
     dp = DataPreparer()
@@ -26,7 +30,7 @@ def main(cfg):
     
     # currently passing dp eventually we want to abstract all the constants into 1 class
     dm = DataManager(df, dp)
-    dm.train_portion = 0.8
+    dm.train_portion = cfg['train_portion']
     
     """ prepare PCDNNV2 for loading (from prior experiments) """
     exprExec = PCDNNV2ExperimentExecutor()
@@ -38,7 +42,7 @@ def main(cfg):
     dataSetMethod = f'{inputType}_{dataType}_{dependants}'
     ipscaler=cfg['ipscaler']#trial.suggest_categorical('input_scaler', scalers_types) #None#'MaxAbsScaler' #"MinMaxScaler"
     opscaler=cfg['opscaler']#trial.suggest_categorical('output_scaler', scalers_types[:-1]) # QuantileTransformer is invalid
-    assert opscaler!='QuantileTransformer'
+    assert opscaler!='QuantileTransformer' and ipscaler!='QuantileTransformer'
     ZmixPresent = cfg['zmix'] 
     concatenateZmix = 'Y' if ZmixPresent=='Y' else 'N'
     kernel_constraint = cfg['kernel_constraint']#trial.suggest_categorical('kernel_constraint', ['Y', 'N'])#'N'
@@ -54,7 +58,7 @@ def main(cfg):
     exprExec.modelFactory.use_R2_losses = exprExec.modelFactory.loss=='R2'
     exprExec.modelFactory.batch_norm_dynamic_pred = cfg['batch_norm_dynamic']#trial.suggest_categorical('batch_norm_dynamic', [True, False]) 
     exprExec.modelFactory.loss_weights = cfg['loss_weights'] 
-
+    exprExec.modelFactory.W_batch_norm = cfg['W_batch_norm']
         
     exprExec.debug_mode = False
     exprExec.batch_size = cfg['batch_size'] 
@@ -85,21 +89,28 @@ def main(cfg):
     print(final_score)
     print(val_R2s)
     print(history)
+    main.exprExec = exprExec # this may be of interest to the caller
     return final_score
 
-import traceback
+# you override these values based with the config values you pass (via dict.update())
+main.default_cfg = {'zmix': 'N', 'ipscaler': None, 'opscaler': 'MinMaxScaler', 'noOfCpv': 4, 'loss': 'R2',
+                    'activation': 'selu', 'width': 512, 'dropout_rate': 0.0, 'batch_norm_dynamic': False,
+                    'kernel_constraint': 'N', 'kernel_regularizer': 'N', 'activity_regularizer': 'N', 'batch_size': 256,
+                    'loss_weights': {'static_source_prediction': 1.0, 'dynamic_source_prediction': 1.0}}
+main.default_cfg.update({'W_batch_norm': False, 'epochs': 1 if debug_mode else 500, 'train_portion': 0.8})
+# add variables generally held as constant
 
+# wrapper for main use in optuna (protects against crashes & uses trials to populate cfg dict)
+import traceback
 def main_safe(trial=None):
     # default config
-    cfg = {'zmix': 'N', 'ipscaler': None, 'opscaler': 'MinMaxScaler', 'noOfCpv': 4, 'loss': 'R2',
-           'activation': 'selu', 'width': 512, 'dropout_rate': 0.0, 'batch_norm_dynamic': True,
-           'kernel_constraint': 'N', 'kernel_regularizer': 'N', 'activity_regularizer': 'N', 'batch_size': 256,
-           'loss_weights': {'static_source_prediction': 1.0, 'dynamic_source_prediction': 1.0}}
+    cfg = {}
+
     if trial:
-        scalers_types = [None, 'MinMaxScaler', 'MaxAbsScaler', 'StandardScaler', 'RobustScaler','QuantileTransformer']
+        scalers_types = [None, 'MinMaxScaler', 'MaxAbsScaler', 'StandardScaler', 'RobustScaler']#,'QuantileTransformer']
 
         cfg = {'zmix': trial.suggest_categorical('zmix', ['Y', 'N']), 'ipscaler': trial.suggest_categorical('input_scaler', scalers_types),
-               'opscaler': trial.suggest_categorical('output_scaler', scalers_types[:-1]), 'noOfCpv': trial.suggest_int('noOfCpv', *[3, 10]),
+               'opscaler': trial.suggest_categorical('output_scaler', scalers_types), 'noOfCpv': trial.suggest_int('noOfCpv', *[3, 10]),
                'loss': trial.suggest_categorical('loss', ['mae', 'mse', 'R2']), 'activation': trial.suggest_categorical('activation', ['selu', 'relu']),
                'width': trial.suggest_int('width', *[256, 1024]), 'dropout_rate': trial.suggest_float('dropout_rate', *[0, 0.4]),
                'batch_norm_dynamic': trial.suggest_categorical('batch_norm_dynamic', [True, False]),
