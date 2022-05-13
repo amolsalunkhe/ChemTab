17c60ef1a96fccbe6a67f692ca5349683fa95612
Dwyer Deighan <dwyerdei@buffalo.edu>
2022-05-13 17:54:40 +0000

    updated optuna_train.py to always use Zmix & orthogonality constraint (this ensures explainability of original species)

--- src/optuna_train.py
+++ src/optuna_train.py
@@ -95,14 +95,15 @@ def main(cfg={}):
     return final_score
 
 # you override these values based with the config values you pass (via dict.update())
-main.default_cfg = {'zmix': 'N', 'ipscaler': None, 'opscaler': 'MinMaxScaler', 'noOfCpv': 4, 'loss': 'R2',
-                    'activation': 'selu', 'width': 512, 'dropout_rate': 0.0, 'batch_norm_dynamic': False,
-                    'batch_size': 256, 'kernel_regularizer': 'N', 'activity_regularizer': 'N', #'kernel_constraint': 'N', 
+main.default_cfg = {'opscaler': 'MinMaxScaler', 'noOfCpv': 4, 'loss': 'R2',
+                    'activation': 'selu', 'width': 512, 'dropout_rate': 0.0,
+                    'batch_size': 256, 'activity_regularizer': 'N', #'kernel_regularizer': 'N', #'kernel_constraint': 'N', 
                     'loss_weights': {'static_source_prediction': 1.0, 'dynamic_source_prediction': 1.0}}
 constants = {'epochs': 10 if debug_mode else 500, 'train_portion': 0.8, 'n_models_override': 1,
-             'use_dynamic_pred': True, 'use_dependants': True, 'data_fn': #'../wax_master_simit.csv',
-             '../NewData_flames_data_with_L1_L2_errors_CH4-AIR_without_trimming(SouSpec_Included).txt',
-             'W_batch_norm': False, 'kernel_constraint': 'N'}
+             'use_dynamic_pred': True, 'use_dependants': True, 'data_fn': '../wax_master_simit.csv',
+             #'../NewData_flames_data_with_L1_L2_errors_CH4-AIR_without_trimming(SouSpec_Included).txt',
+             'kernel_constraint': 'N', 'kernel_regularizer': 'Y', 'zmix': 'Y', 
+             'ipscaler': None, 'W_batch_norm': False, 'batch_norm_dynamic': False} # this line is all garbage configs
 main.default_cfg.update(constants)
 # add variables generally held as constant
 
